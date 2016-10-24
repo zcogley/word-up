0c6f7343b78dc86946b77c682df00871c75d7d32
jesseilev <jesseilev@gmail.com>
2016-10-24 13:40:55 -0500

    better naming scheme

--- scripts/wordup.js
+++ scripts/wordup.js
@@ -2,14 +2,14 @@
 // INITIALIZE STUFF
 
 $(document).ready(function() {
-    $("#attempted-word-form").submit(function(evt) {
+    $("#word-attempt-form").submit(function(evt) {
         evt.preventDefault();
         var word = $("#textbox").val();
         addNewWordSubmission(word);
         render();
     });
 
-    $("#textbox").bind("input", function() {
+    $("#textbox").on("input", function() {
         model.currentAttempt = $("#textbox").val().toLowerCase();
         render();
     });
@@ -30,45 +30,52 @@ var gameDuration = 60;
 var model = {
     gameHasStarted: false,
     secondsRemaining: gameDuration,
-    validChars: [],
+    allowedLetters: [],
     wordSubmissions: [],
     currentAttempt: ""
-}
-
-function addNewWordSubmission(word) {
-    var alreadyUsed = model.wordSubmissions.filter(function(sub) {
-        return sub.word === word;
-    }).length > 0;
-    if (containsOnlyValidChars(word) && !alreadyUsed) {
-        model.wordSubmissions.push({ word: word });
-        checkWordExists(word);
-    }
-    model.currentAttempt = "";
 }
 
 function startGame() {
     endGame();
     model.gameHasStarted = true;
     model.secondsRemaining = gameDuration;
-    model.validChars = generateValidChars();
+    model.allowedLetters = generateAllowedLetters();
     model.wordSubmissions = [];
     model.currentAttempt = "";
-    model.timer = tickTimer();
+    model.timer = startTimer();
 }
 
 function endGame() {
+    stopTimer();
+}
+
+function startTimer() {
+    function tick() {
+        return setTimeout(function() {
+            model.secondsRemaining = Math.max(0, model.secondsRemaining - 1);
+            render();
+            var stillTimeLeft = model.gameHasStarted && model.secondsRemaining > 0
+            if (stillTimeLeft) {
+                model.timer = tick();
+            }
+        }, 1000);
+    }
+    return tick();
+}
+
+function stopTimer() {
     clearTimeout(model.timer);
 }
 
-function tickTimer() {
-    return setTimeout(function() {
-        model.secondsRemaining = Math.max(0, model.secondsRemaining - 1);
-        render();
-        var stillTimeLeft = model.gameHasStarted && model.secondsRemaining > 0
-        if (stillTimeLeft) {
-            model.timer = tickTimer();
-        }
-    }, 1000);
+function addNewWordSubmission(word) {
+    var alreadyUsed = model.wordSubmissions.filter(function(sub) {
+        return sub.word === word;
+    }).length > 0;
+    if (containsOnlyAllowedLetters(word) && !alreadyUsed) {
+        model.wordSubmissions.push({ word: word });
+        checkWordExists(word);
+    }
+    model.currentAttempt = "";
 }
 
 function checkWordExists(word) {
@@ -98,36 +105,47 @@ function render() {
     // clear stuff
     $("#textbox").val("");
     $("#word-submissions").empty();
-    $("#valid-chars").empty();
-    $("#invalid-chars").empty();
+    $("#allowed-letters").empty();
+    $("#disallowed-letters").empty();
 
-    // scoreboard
+    // update the scoreboard
     $("#current-score").text(currentScore());
     $("#time-remaining").text(model.secondsRemaining);
 
+    // if the game has not started yet, just hide the #game container and exit
     if (model.gameHasStarted == false) {
         $("#game").hide();
         return;
     }
 
+    // reveal the #game container
     $("#game").show();
-    $("#valid-chars").append(model.validChars.map(charElem));
-    $("#textbox").attr("disabled", false);
-    $("#textbox").focus();
-    $("#textbox").val(model.currentAttempt);
-    $("#textbox").removeClass("bad-attempt");
 
-    // validate the textbox
-    var invalidCharElems = invalidCharTags(model.currentAttempt)
-    if (invalidCharElems.length > 0) {
-        $("#invalid-chars").append(invalidCharElems);
-        $("#textbox").addClass("bad-attempt");
-    }
+    // render the letter tiles
+    $("#allowed-letters").append(model.allowedLetters.map(letterElem));
 
-    // add the word submissions
+    // render the word submissions
     $("#word-submissions").append(model.wordSubmissions.map(wordElem));
 
-    if (model.secondsRemaining <= 0) {
+    // render the textbox
+    $("#textbox")
+        .val(model.currentAttempt)
+        .focus()
+        .removeClass("bad-attempt")
+        .attr("disabled", false);
+
+    // if the current word attempt contains disallowed letters,
+    // restyle the textbox and show the disallowed letters underneath
+    var disallowedLetters = disallowedLettersInWord(model.currentAttempt);
+    if (disallowedLetters.length > 0) {
+        $("#textbox").addClass("bad-attempt");
+        $("#disallowed-letters")
+            .append(disallowedLetters.map(disallowedLetterElem));
+    }
+
+    // if the game is over, disable the text box and clear its contents
+    var gameOver = model.secondsRemaining <= 0
+    if (gameOver) {
         $("#textbox").prop("disabled", true);
         $("#textbox").val("");
     }
@@ -136,68 +154,69 @@ function render() {
 function wordElem(wordSubmission) {
     var elem = $("<span></span>")
         .text(wordSubmission.word)
-        .attr("class", "tag word-submission");
+        .attr("class", "tag tag-lg word-submission");
     if (wordSubmission.hasOwnProperty("isRealWord")) {
-        var badge = $("<span></span>")
+        var scoreTag = $("<span></span>")
             .text(wordSubmission.isRealWord ? wordScore(wordSubmission.word) : "X")
             .attr("class", "tag tag-sm")
             .addClass(wordSubmission.isRealWord ? "tag-success" : "tag-danger");
-        elem.append(badge);
+        elem.append(scoreTag);
     }
     return elem;
 }
 
-function charElem(char) {
-    var badge = $("<span></span>")
-        .text(charScore(char))
+function letterElem(letter) {
+    var scoreTag = $("<span></span>")
+        .text(letterScore(letter))
         .attr("class", "tag tag-default tag-sm");
     return $("<span></span>")
-        .text(char)
-        .attr("class", "tag valid-char")
-        .append(badge);
+        .text(letter)
+        .attr("class", "tag tag-lg allowed-letter")
+        .append(scoreTag);
 }
 
-function invalidCharTags(word) {
-    return word.split("").filter(isNotValidChar).map(function(char) {
-        return $("<span></span>").text(char).addClass("tag tag-danger");
-    });
+function disallowedLetterElem(letter) {
+    return $("<span></span>").text(letter).addClass("tag tag-sm tag-danger");
 }
 
 
 
 // GAME LOGIC
 
-var scrabbleTiles = {
+var scrabblePointsForEachLetter = {
     a: 1, b: 3, c: 3, d: 2, e: 1, f: 4, g: 2, h: 4, i: 1, j: 8, k: 5, l: 1, m: 3,
     n: 1, o: 1, p: 3, q: 10, r: 1, s: 1, t: 1, u: 1, v: 4, w: 4, x: 8, y: 4, z: 10
 }
 
-function isValidChar(char) {
-    return model.validChars.indexOf(char) !== -1
+function isAllowedLetter(letter) {
+    return model.allowedLetters.indexOf(letter) !== -1
 }
 
-function isNotValidChar(char) {
-    return isValidChar(char) == false;
+function isDisallowedLetter(letter) {
+    return isAllowedLetter(letter) == false;
 }
 
-function containsOnlyValidChars(word) {
-    var chars = word.split("");
-    return chars.filter(isValidChar).length === chars.length;
+function containsOnlyAllowedLetters(word) {
+    return disallowedLettersInWord(word).length == 0;
 }
 
-function generateValidChars() {
-    return chooseN(7, Object.keys(scrabbleTiles));
+function disallowedLettersInWord(word) {
+    return word.split("").filter(isDisallowedLetter);
+}
+
+function generateAllowedLetters() {
+    return chooseN(7, Object.keys(scrabblePointsForEachLetter));
 }
 
 function wordScore(word) {
-    if (containsOnlyValidChars(word) == false) {
+    if (containsOnlyAllowedLetters(word) == false) {
         return 0;
     }
-    return word.split("").map(charScore).reduce(add, 0);
+    return word.split("").map(letterScore).reduce(add, 0);
 }
 
-function charScore(char) {
-    return scrabbleTiles[char.toLowerCase()];
+function letterScore(letter) {
+    return scrabblePointsForEachLetter[letter.toLowerCase()];
 }
 
 function currentScore() {
@@ -209,16 +228,24 @@ function currentScore() {
 
 // UTILS
 
-function add(a, b) {
-    return a + b;
-}
-
+/**
+ * randomly selects n items from a list,
+ * and returns the selected items together in a smaller list
+ */
 function chooseN(n, items) {
-    var choices = [];
-    for (var i = 0; i < n; i++) {
+    var selectedItems = [];
+    var total = Math.min(n, items.length)
+    for (var i = 0; i < total; i++) {
         index = Math.floor(Math.random() * items.length);
-        choices.push(items[index]);
+        selectedItems.push(items[index]);
         items.splice(index, 1);
     }
-    return choices;
+    return selectedItems;
+}
+
+/**
+ * adds two numbers together
+ */
+function add(a, b) {
+    return a + b;
 }
