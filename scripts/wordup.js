f805a6882864d3841647c201bcc3c61177f3f11c
Zac Cogley <zaccogley@gmail.com>
2017-04-10 20:25:59 -0500

    implement wordscore function

--- scripts/wordup.js
+++ scripts/wordup.js
@@ -362,7 +362,7 @@ function wordScore(word) {
     // TODO 19
     // Replace the empty list below.
     // Map the list of letters into a list of scores, one for each letter.
-  const letterScores = [];
+  const letterScores = letters.map(letterScore);
 
     // return the total sum of the letter scores
   return letterScores.reduce(add, 0);
